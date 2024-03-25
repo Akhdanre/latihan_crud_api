30786b1d3ad0e0ad9ac1a882164b756a8f06efc1
Akeon <danakhdan12@gmail.com>
2024-03-25 23:32:56 +0700

    add endpoint update and delete data

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000
 
 app.use(express.json())
 
-app.get("/api/products", async function (req, res) {
+app.get("/api/products", async function (req, res, next) {
     let id = req.query.id
     let query, data
     if (id) {
@@ -30,7 +30,7 @@ app.get("/api/products", async function (req, res) {
     }
 })
 
-app.post("/api/products", async function (req, res) {
+app.post("/api/products", async function (req, res, next) {
     let request = req.body
     try {
         let query = "INSERT INTO products (name, price, category) VALUES ($1, $2, $3)"
@@ -42,12 +42,22 @@ app.post("/api/products", async function (req, res) {
     }
 })
 
-app.put("/api/products/:id", async function (req, res) {
+app.put("/api/products/:id", async function (req, res, next) {
     let productId = req.params.id
     let request = req.body
     try {
-        let query = "UPDATE TABLE "
-        let data = await db.newDb().query(query, [request.name, request.price, request.category])
+        let query = "UPDATE products SET name = $1, price = $2, category = $3, is_available = $4 WHERE id = $5"
+        let data = await db.newDb().query(query, [request.name, request.price, request.category, request.is_available, productId])
+        res.json(data.rows)
+    } catch (err) {
+        next(err)
+    }
+})
+app.delete("/api/products/:id", async function (req, res, next) {
+    let productId = req.params.id
+    try {
+        let query = "DELETE FROM products WHERE id = $1"
+        let data = await db.newDb().query(query, [productId])
         res.json(data.rows)
     } catch (err) {
         next(err)
