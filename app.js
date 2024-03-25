216848834b17199034fa6f94c93b9c2c7971b043
Akeon <danakhdan12@gmail.com>
2024-03-25 23:53:19 +0700

    fix : adjust webresponse

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const port = 3000
 
 app.use(express.json())
 
+function webResponse(code, message, data) {
+    return {
+        code: code,
+        message: message,
+        data: data
+    }
+}
+
 app.get("/api/products", async function (req, res, next) {
     let id = req.query.id
     let query, data
@@ -13,7 +21,7 @@ app.get("/api/products", async function (req, res, next) {
         try {
             query = "SELECT * FROM products WHERE id = $1"
             data = await db.newDb().query(query, [id])
-            res.json(data.rows)
+            res.json(webResponse(200, "success", data.rows))
         } catch (err) {
             next(err)
         }
@@ -21,7 +29,7 @@ app.get("/api/products", async function (req, res, next) {
         try {
             query = "SELECT * FROM products"
             data = await db.newDb().query(query)
-            res.json(data.rows)
+            res.json(webResponse(200, "success", data.rows))
         } catch (err) {
             next(err)
         }
@@ -33,7 +41,7 @@ app.post("/api/products", async function (req, res, next) {
     try {
         let query = "INSERT INTO products (name, price, category) VALUES ($1, $2, $3)"
         let data = await db.newDb().query(query, [request.name, request.price, request.category])
-        res.json(data.rows)
+        res.json(webResponse(200, "success", data.rows))
     }
     catch (err) {
         next(err)
@@ -46,7 +54,7 @@ app.put("/api/products/:id", async function (req, res, next) {
     try {
         let query = "UPDATE products SET name = $1, price = $2, category = $3, is_available = $4 WHERE id = $5"
         let data = await db.newDb().query(query, [request.name, request.price, request.category, request.is_available, productId])
-        res.json(data.rows)
+        res.json(webResponse(200, "success", data.rows))
     } catch (err) {
         next(err)
     }
@@ -56,7 +64,7 @@ app.delete("/api/products/:id", async function (req, res, next) {
     try {
         let query = "DELETE FROM products WHERE id = $1"
         let data = await db.newDb().query(query, [productId])
-        res.json(data.rows)
+        res.json(webResponse(200, "success", data.rows))
     } catch (err) {
         next(err)
     }
@@ -66,12 +74,12 @@ app.delete("/api/products/:id", async function (req, res, next) {
 // internal error
 app.use((err, req, res, next) => {
     console.log(err)
-    res.status(500).json({ message: "some thing wrong" })
+    res.status(500).json(webResponse(500, "SOMETHING WRONG IN SERVER", null))
 })
 
 // 404 not found endpoint
 app.use((req, res, next) => {
-    res.status(404).json({ message: `are you lost? ${req.method} ${req.url} is not registered` })
+    res.status(404).json(webResponse(404, "ENDPOINT NOT FOUND", null))
 })
 
 app.listen(port, function () {
