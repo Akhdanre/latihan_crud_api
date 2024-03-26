7d23f72f15b266e315fc21dd149d98c7c982c391
Akeon <danakhdan12@gmail.com>
2024-03-26 21:15:30 +0700

    fix : adjust minor on product update and delete

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.put("/api/products/:id", async function (req, res, next) {
     try {
         let query = "UPDATE products SET name = $1, price = $2, category = $3, is_available = $4 WHERE id = $5"
         let data = await db.newDb().query(query, [request.name, request.price, request.category, request.is_available, productId])
-        res.json(webResponse(200, "success", data.rows))
+        res.json(webResponse(200, "success", data.rowCount))
     } catch (err) {
         next(err)
     }
@@ -64,7 +64,7 @@ app.delete("/api/products/:id", async function (req, res, next) {
     try {
         let query = "DELETE FROM products WHERE id = $1"
         let data = await db.newDb().query(query, [productId])
-        res.json(webResponse(200, "success", data.rows))
+        res.json(webResponse(200, "success", data.rowCount))
     } catch (err) {
         next(err)
     }
